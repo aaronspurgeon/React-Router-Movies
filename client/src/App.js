36b9de9c0401c296afc89fe6a7af3715b5daa38f
Aaron Spurgeon <aspurgeon061@gmail.com>
2019-09-05 20:50:47 -0400

    having trouble getting my info displayed

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Route } from 'react-router-dom';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie';
 import SavedList from './Movies/SavedList';
@@ -14,7 +15,8 @@ const App = () => {
     <div>
       <SavedList list={savedList} />
       <Route exact path="/" component={MovieList} />
-      <Route exact path="/movies/:id" component={Movie} />
+      <Route path="/movies/:id" 
+      render={props => <Movie {...props} />} />
     </div>
   );
 };
